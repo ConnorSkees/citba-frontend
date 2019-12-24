8a5dde9296c127fc13980ea5f6ab3e5bee876cf2
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-12-24 10:34:37 -0500

    Updates

---
+++ precache-manifest.d88c886c86d92faf8ee3c7a4927a999d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627856243bbf12ac3d212ade0d2e8396",
+    "url": "/citba-frontend/index.html"
+  },
+  {
+    "revision": "3b1674a462e36dca3c3c",
+    "url": "/citba-frontend/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "7cdc2ff2059bcce50101",
+    "url": "/citba-frontend/static/css/main.783a4015.chunk.css"
+  },
+  {
+    "revision": "3b1674a462e36dca3c3c",
+    "url": "/citba-frontend/static/js/2.dff78d88.chunk.js"
+  },
+  {
+    "revision": "0fdce6425be199f2fc7d2b26422d5b99",
+    "url": "/citba-frontend/static/js/2.dff78d88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cdc2ff2059bcce50101",
+    "url": "/citba-frontend/static/js/main.39c97204.chunk.js"
+  },
+  {
+    "revision": "5663842b9cabad79e343",
+    "url": "/citba-frontend/static/js/runtime-main.3c7ddfc4.js"
+  },
+  {
+    "revision": "3883931ac4e3321c2c7fad356126bbc9",
+    "url": "/citba-frontend/static/media/arrow-down.3883931a.svg"
+  },
+  {
+    "revision": "2436f68137c646226f69b953611d778c",
+    "url": "/citba-frontend/static/media/arrow-left.2436f681.svg"
+  },
+  {
+    "revision": "d9e9084cba3933893bddec58404adc4e",
+    "url": "/citba-frontend/static/media/arrow-right.d9e9084c.svg"
+  },
+  {
+    "revision": "6cf75e21cb6d85821f4e6de01220f224",
+    "url": "/citba-frontend/static/media/awards.6cf75e21.jpg"
+  },
+  {
+    "revision": "3cf357f8087d871cad07a744c8378fc9",
+    "url": "/citba-frontend/static/media/balance-scale.3cf357f8.svg"
+  },
+  {
+    "revision": "f8fb6abe752821d24dc24e4edc75b1f6",
+    "url": "/citba-frontend/static/media/board-of-directors.f8fb6abe.svg"
+  },
+  {
+    "revision": "2b5c529ee772e674c0756a6a49918370",
+    "url": "/citba-frontend/static/media/board-room.2b5c529e.jpg"
+  },
+  {
+    "revision": "2349d5fed79e765aaa85a509f35d6479",
+    "url": "/citba-frontend/static/media/book.2349d5fe.svg"
+  },
+  {
+    "revision": "591a8bd8c246004f63da9cf0341c328d",
+    "url": "/citba-frontend/static/media/briefcase.591a8bd8.svg"
+  },
+  {
+    "revision": "c0b21d6b3e39236fe968e96deedf65f6",
+    "url": "/citba-frontend/static/media/carousel-image-1.c0b21d6b.jpg"
+  },
+  {
+    "revision": "dc53d0f69195e930b014770d5749d3b4",
+    "url": "/citba-frontend/static/media/carousel-image-2.dc53d0f6.jpg"
+  },
+  {
+    "revision": "f5bf9f79c328295626f93f8ef89298db",
+    "url": "/citba-frontend/static/media/carousel-image-3.f5bf9f79.jpg"
+  },
+  {
+    "revision": "874ca3cdeca723e7e36ae60c734a7fec",
+    "url": "/citba-frontend/static/media/citba-footer.874ca3cd.png"
+  },
+  {
+    "revision": "e18baee82f76d7247a343e5182bd19c8",
+    "url": "/citba-frontend/static/media/committees.e18baee8.svg"
+  },
+  {
+    "revision": "b6f24e602edf98183f5fae8375ab51b3",
+    "url": "/citba-frontend/static/media/email.b6f24e60.svg"
+  },
+  {
+    "revision": "cdc4bec36c0ea247cb7c9d6ed01ecbc0",
+    "url": "/citba-frontend/static/media/file-pdf.cdc4bec3.svg"
+  },
+  {
+    "revision": "1aab12d8662f0dfd91cb96a0170d1c69",
+    "url": "/citba-frontend/static/media/gavel.1aab12d8.svg"
+  },
+  {
+    "revision": "5924881cdb4163556797480ce5af5774",
+    "url": "/citba-frontend/static/media/home.5924881c.svg"
+  },
+  {
+    "revision": "86d23f47e8166ac627228d349a774d63",
+    "url": "/citba-frontend/static/media/library.86d23f47.jpg"
+  },
+  {
+    "revision": "402465d98d713da4f259fb6c604b7cb9",
+    "url": "/citba-frontend/static/media/man-with-papers.402465d9.jpg"
+  },
+  {
+    "revision": "fb3569096a8686cd86bf31a69dd90a9a",
+    "url": "/citba-frontend/static/media/man-writing.fb356909.jpg"
+  },
+  {
+    "revision": "0e6d84c54cda4105cd7df6f6f42eea7b",
+    "url": "/citba-frontend/static/media/map.0e6d84c5.jpg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/citba-frontend/static/media/minus.777b93c6.svg"
+  },
+  {
+    "revision": "7aec8849539fdea92379076b274ee9d0",
+    "url": "/citba-frontend/static/media/newspaper.7aec8849.svg"
+  },
+  {
+    "revision": "c80a59ebd7d8ab76ecd3a11c936e2540",
+    "url": "/citba-frontend/static/media/plus.c80a59eb.svg"
+  },
+  {
+    "revision": "c822cb13260a27d7247009c228627ee8",
+    "url": "/citba-frontend/static/media/richard-belanger-2019.c822cb13.jpg"
+  },
+  {
+    "revision": "692fd16959c1655595bdebb014ff26af",
+    "url": "/citba-frontend/static/media/scales.692fd169.jpg"
+  },
+  {
+    "revision": "ed9c63ff5988c423fde738dd7101f878",
+    "url": "/citba-frontend/static/media/twitter-logo.ed9c63ff.svg"
+  },
+  {
+    "revision": "2fe2dffd9293fad0260696a34cc8d40b",
+    "url": "/citba-frontend/static/media/woman-typing.2fe2dffd.jpg"
+  }
+]);
